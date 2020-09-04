d6c34b627488dc8e2ed72bf5482db1ef18eacb67
TanJunHong <e0634975@u.nus.edu>
2020-09-04 22:49:33 +0800

    Add error message

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,3 +1,8 @@
 class DukeException extends Exception {
 
+    DukeException(String errorMessage) {
+        System.out.println("\t" + "____________________________________________________________");
+        System.out.println("\t " + errorMessage);
+        System.out.println("\t" + "____________________________________________________________");
+    }
 }
