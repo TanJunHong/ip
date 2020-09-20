1ccc14559929a08905675dcfd3b61a972fb03229
TanJunHong <e0634975@u.nus.edu>
2020-09-20 18:46:20 +0800

    Teach Duke to understand date and time

--- src/main/java/duke/data/task/Event.java
+++ src/main/java/duke/data/task/Event.java
@@ -1,24 +1,33 @@
 package duke.data.task;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
     public static final String LOGO = "E";
     public static final String DELIMITER = "/at";
 
-    private final String at;
+    public Event(String description, boolean isDone, LocalDate date, LocalTime time) {
+        super(description, isDone, date, time);
+    }
 
-    public Event(String description, boolean isDone, String at) {
-        super(description, isDone);
-        this.at = at;
+    private String getAtDateString() {
+        return getDate().format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
+
+    private String getAtTimeString() {
+        return getTime() == null ? "" : " " + getTime().toString();
     }
 
     @Override
     public String getFormattedTask() {
-        return LOGO + super.getFormattedTask() + " | " + at + System.lineSeparator();
+        return LOGO + super.getFormattedTask() + " | " + getDate() + getAtTimeString() + System.lineSeparator();
     }
 
     @Override
     public String toString() {
-        return "[" + LOGO + "]" + super.toString() + " (at: " + at + ")";
+        return "[" + LOGO + "]" + super.toString() + " (at: " + getAtDateString() + getAtTimeString() + ")";
     }
 }
--- src/main/java/duke/data/task/Task.java
+++ src/main/java/duke/data/task/Task.java
@@ -1,21 +1,37 @@
 package duke.data.task;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public abstract class Task {
 
     public static final String COMPLETE = "1";
     public static final String INCOMPLETE = "0";
     private static final String TICK = "[✓]";
     private static final String CROSS = "[✗]";
+
     private final String description;
     private boolean isDone;
+    private final LocalDate date;
+    private final LocalTime time;
 
-    Task(String description, boolean isDone) {
+    Task(String description, boolean isDone, LocalDate date, LocalTime time) {
         this.description = description;
         this.isDone = isDone;
+        this.date = date;
+        this.time = time;
     }
 
     public void markAsDone() {
         this.isDone = true;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
     }
 
     private String getDescription() {
